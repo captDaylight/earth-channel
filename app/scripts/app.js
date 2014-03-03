143d44f3544b8274bad897f37b616d20cffb5acb
Paul Christophe <paulchristophe@Pauls-MacBook-Air.local>
2014-03-03 17:59:07 -0500

    adding the views and routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,12 +8,28 @@ angular.module('champagneRocksApp', [
 ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'partials/main',
+      // .when('/', {
+      //   templateUrl: 'partials/main',
+      //   controller: 'MainCtrl'
+      // })
+      .when('/interactive',{
+        templateUrl: 'partials/interactive',
+        controller: 'InteractiveCtrl'
+      })
+      .when('/record',{
+        templateUrl: 'partials/record',
+        controller: 'MainCtrl'
+      })
+      .when('/cover',{
+        templateUrl: 'partials/cover',
+        controller: 'MainCtrl'
+      })
+      .when('/travel',{
+        templateUrl: 'partials/travel',
         controller: 'MainCtrl'
       })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/record'
       });
       
     $locationProvider.html5Mode(true);
