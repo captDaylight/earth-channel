9c309ee6d7b40d2db522360738bb7fc3a2472c34
Paul Christophe <paulchristophe@Pauls-MacBook-Air.local>
2014-03-04 19:41:15 -0500

    traveling visual

--- app/scripts/controllers/travel.js
+++ app/scripts/controllers/travel.js
@@ -24,62 +24,65 @@ console.log('travel controller');
 	    group.position.y = -100;
 	    scene.add( group );
 
+
+	    // add ground
+	    var groundMaterial = new THREE.MeshPhongMaterial( { ambient: 0x030303, color: 0xdddddd, specular: 0x009900, shininess: 30, shading: THREE.FlatShading } )
+		var mesh = new THREE.Mesh( new THREE.PlaneGeometry( 20000, 20000, 50, 50 ), groundMaterial );
+		mesh.position.y = -750;
+		mesh.rotation.x = -1.5;
+		// mesh.position.z = -2000;
+		scene.add( mesh );
+
 	    renderer = new THREE.CanvasRenderer();
-	    renderer.setClearColor( 0xf0f0f0 );
+	    renderer.setClearColor( 0x666666 );
 	    renderer.setSize(window.innerWidth, window.innerHeight);
 
 	    document.body.appendChild(renderer.domElement);
+	    console.log(scene.__objectsAdded[1]);
 
-	}
-
-	function createCircle(timer){
-	    
-	    var circleRadius = 100 + (Math.cos(timer) * 20);
-	    var circleShape = new THREE.Shape();
-	    circleShape.moveTo( 0, circleRadius );
-	    circleShape.quadraticCurveTo( circleRadius, circleRadius, circleRadius, 0 );
-	    circleShape.quadraticCurveTo( circleRadius, -circleRadius, 0, -circleRadius );
-	    circleShape.quadraticCurveTo( -circleRadius, -circleRadius, -circleRadius, 0 );
-	    circleShape.quadraticCurveTo( -circleRadius, circleRadius, 0, circleRadius );
-	    return circleShape;
 	}
 
 	function addShape( shape, color, x, y, z, rx, ry, rz, s ) {
 
 	    // // flat shape
 
-	    var geometry = new THREE.ShapeGeometry( shape );
-	    var material = new THREE.MeshBasicMaterial( { color: color, overdraw: true } );
+	    // var geometry = new THREE.ShapeGeometry( shape );
+	    // var material = new THREE.MeshBasicMaterial( { color: color, overdraw: true } );
 
-	    var mesh = new THREE.Mesh( geometry, material );
-	    mesh.position.set( x, y, z );
-	    mesh.rotation.set( rx, ry, rz );
-	    mesh.scale.set( s, s, s );
-	    group.add( mesh );
+	    // var mesh = new THREE.Mesh( geometry, material );
+	    // mesh.position.set( x, y, z );
+	    // mesh.rotation.set( rx, ry, rz );
+	    // mesh.scale.set( s, s, s );
+	    // group.add( mesh );
 
 	    // // line
 
 	    var geometry = shape.createPointsGeometry();
-	    var material = new THREE.LineBasicMaterial( { linewidth: 1, color: 0x333333, transparent: true } );
+	    var material = new THREE.LineBasicMaterial( { linewidth:2, color: color, transparent: true } );
 
 	    var line = new THREE.Line( geometry, material );
 	    line.position.set( x, y, z );
 	    line.rotation.set( rx, ry, rz );
 	    line.scale.set( s, s, s );
-	    group.add( line );
-
+	    // group.add( line );
+	    scene.add(line);
 	}
 
 	var xPos = 0;
-	var radius = 500;
+	var radius = 300;
 	var rotation = 0;
 
 	var mouseX = 0;
 	var mouseY = 0;
 
 	function animate() {
-	    var timer = 0.001 * Date.now();
+	    var timer = 0.005 * Date.now();
 
+	    //removing objects from the scene, to lighten the load
+	  	if(scene.__objectsAdded.length > 120 ){
+	  		// 3 signifies not to delete the base objects like ground plane
+	  		scene.remove(scene.__objectsAdded[3]);
+	  	}
 
 	    function componentToHex(c) {
 	        var hex = c.toString(16);
@@ -96,18 +99,38 @@ console.log('travel controller');
 	        color: new THREE.Color( Math.cos(timer), Math.sin(timer), -Math.sin(timer) )
 	    });
 
-	    var segments = 3;
 
-	    var circleGeometry = new THREE.CircleGeometry( radius + (Math.cos(timer) * 20), segments );              
-	    var circle = new THREE.Mesh( circleGeometry, material );
 
-	    circle.position.x = mouseX;
-	    circle.position.y = mouseY;
-	    circle.position.z = xPos*10;
-	    circle.rotation.z = rotation;
-	    rotation += .1;
-	    xPos += 1;
-	    scene.add( circle );
+				// Rectangle
+
+		var rectLength = 200, rectWidth = 200;
+
+		var rectShape = new THREE.Shape();
+		rectShape.moveTo( 0,0 );
+		rectShape.lineTo( 0, rectWidth );
+		rectShape.lineTo( rectLength, rectWidth );
+		rectShape.lineTo( rectLength, 0 );
+		rectShape.lineTo( 0, 0 );
+
+		// addShape( rectShape, 0x005500, -150, 150, 0, 0, 0, 0, 1 );
+		addShape( rectShape, 0xffffff, mouseX, mouseY, xPos*10, 0, 0, 0, 1 );
+
+	    // // create triangle from circle generator
+	    // var segments = 20;
+
+	    // var circleGeometry = new THREE.CircleGeometry( radius + (Math.cos(timer) * 100), segments );              
+	    // var circle = new THREE.Mesh( circleGeometry, material );
+
+	    // circle.position.x = mouseX;
+	    // circle.position.y = mouseY;
+	    // circle.position.z = xPos*10;
+	    // circle.rotation.z = rotation;
+	    // rotation += .1;
+	    // xPos += 1;
+	    // scene.add( circle );
+
+
+		xPos += 1;
 
 
 	    // note: three.js includes requestAnimationFrame shim
@@ -118,6 +141,7 @@ console.log('travel controller');
 
 	    renderer.render(scene, camera);
 	    camera.position.z += 10;
+
 	}
 
 	var windowHalfX = window.innerWidth / 2;
